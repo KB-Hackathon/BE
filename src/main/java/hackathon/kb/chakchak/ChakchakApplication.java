0559f0d5416c10da8c58bec8d181ee1f0bf0a520
greensnapback0229 <smdmim@gmail.com>
2025-08-30 02:49:43 +0900

    feat: baseEntity 설정

--- src/main/java/hackathon/kb/chakchak/ChakchakApplication.java
+++ src/main/java/hackathon/kb/chakchak/ChakchakApplication.java
@@ -2,7 +2,9 @@ package hackathon.kb.chakchak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ChakchakApplication {
 
