4080f9a6058349912a1281cc69862e8ca7337b69
junbeom <jach1206@naver.com>
2025-09-02 17:45:39 +0900

    feat: 카프카로 로그를 보내도록 에러 핸들러 수정

--- src/main/java/hackathon/kb/chakchak/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/hackathon/kb/chakchak/global/exception/handler/GlobalExceptionHandler.java
@@ -5,12 +5,29 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import hackathon.kb.chakchak.global.exception.exceptions.BusinessException;
+import hackathon.kb.chakchak.global.kafka.dto.Common;
+import hackathon.kb.chakchak.global.kafka.producer.KafkaProducer;
+import hackathon.kb.chakchak.global.kafka.util.LogMessageMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
+	private final KafkaProducer kafkaProducer;
+
 	@ExceptionHandler(BusinessException.class)
-	public ResponseEntity<String> handleBusinessException(BusinessException e) {
+	public ResponseEntity<String> handleBusinessException(BusinessException e, HttpServletRequest request) {
+		kafkaProducer.sendToLogTopic(LogMessageMapper.buildLogMessage(
+			e.getLogLevel(),
+			e.getTxId(),
+			e.getResponseCode().getMessage(),
+			Common.builder().srcIp(request.getRemoteAddr()).apiMethod(request.getMethod())
+				.callApiPath(request.getRequestURI()).deviceInfo(request.getHeader("user-agent"))
+				.retryCount(0).build(),
+			e.getMessage()
+		));
 		return ResponseEntity.status(e.getResponseCode().getStatus()).body(e.getMessage());
 	}
 }
