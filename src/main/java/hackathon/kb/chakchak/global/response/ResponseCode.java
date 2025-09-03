d51764f9a9182641f1ce68447730f1148fb3ac9f
junbeom <jach1206@naver.com>
2025-09-04 00:15:47 +0900

    feat: S3 에러코드 추가

--- src/main/java/hackathon/kb/chakchak/global/response/ResponseCode.java
+++ src/main/java/hackathon/kb/chakchak/global/response/ResponseCode.java
@@ -15,7 +15,11 @@ public enum ResponseCode {
 	UNAUTHORIZED("GEN-002", HttpStatus.UNAUTHORIZED, "Unauthorized"),
 	NOT_FOUND("GEN-003", HttpStatus.NOT_FOUND, "Not Found"),
 	CONFLICT("GEN-004", HttpStatus.CONFLICT, "Conflict"),
-	INTERNAL_SERVER_ERROR("GEN-005", HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
+	INTERNAL_SERVER_ERROR("GEN-005", HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
+	S3_UPLOAD_FAIL("GEN-006", HttpStatus.INTERNAL_SERVER_ERROR, "S3 이미지 업로드 실패"),
+	S3_NOT_FOUND("GEN-007", HttpStatus.INTERNAL_SERVER_ERROR, "이름 기반 S3 이미지 조회 실패"),
+
+	;
 
 
 	private final String code;
