4ed8dc1bca8dfc6592f3760160808019c815be1a
junbeom <jach1206@naver.com>
2025-09-05 17:14:42 +0900

    feat: 상품 조회에 필요한 컨트롤러 계층 코드 작성

--- src/main/java/hackathon/kb/chakchak/domain/product/api/controller/ProductController.java
+++ src/main/java/hackathon/kb/chakchak/domain/product/api/controller/ProductController.java
@@ -1,11 +1,20 @@
 package hackathon.kb.chakchak.domain.product.api.controller;
 
+import hackathon.kb.chakchak.domain.product.domain.dto.ProductReadResponseDto;
+import hackathon.kb.chakchak.domain.product.domain.enums.Category;
 import hackathon.kb.chakchak.domain.product.service.OpenAIMultimodalNarrativeService;
+import hackathon.kb.chakchak.domain.product.service.ProductBasicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,8 +22,10 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/product")
+@Tag(name = "상품 조회 API", description = "상품 조회용 API")
 public class ProductController {
     private final OpenAIMultimodalNarrativeService narrativeService;
+    private final ProductBasicService productBasicService;
 
     @PostMapping("/narrative")
     public String makeNarrative(@RequestBody NarrativeRequest req) {
@@ -23,6 +34,20 @@ public class ProductController {
                 req.getUserText(),
                 req.getImageUrls()
         );
+    }
+
+    @Operation(summary = "카테고리별 조회", description = "카테고리별 상품을 조회합니다. 10개를 반환하도록 하였습니다.")
+    @GetMapping("/category/{category}")
+    public List<ProductReadResponseDto> listByCategory(
+        @PathVariable Category category,
+        @RequestParam(defaultValue = "0") int page) {
+        return productBasicService.getProductsByCategory(category, page);
+    }
+
+    @Operation(summary = "상품 아이디 기반 조회", description = "상품 아이디를 기반으로 한 개 상품의 정보를 조회합니다.")
+    @GetMapping("/{productId}")
+    public ProductReadResponseDto listByCategory(@PathVariable(name = "productId") Long productId){
+        return productBasicService.getProductById(productId);
     }
 
     // 임시용
