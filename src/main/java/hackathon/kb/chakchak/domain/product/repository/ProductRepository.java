b9784261a7aba787e59ff6feba1f9613a91b2e81
junbeom <jach1206@naver.com>
2025-09-05 17:14:16 +0900

    feat: 조회 필요 쿼리 작성

---
+++ src/main/java/hackathon/kb/chakchak/domain/product/repository/ProductRepository.java
@@ -0,0 +1,28 @@
+package hackathon.kb.chakchak.domain.product.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import hackathon.kb.chakchak.domain.product.domain.entity.Product;
+import hackathon.kb.chakchak.domain.product.domain.enums.Category;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	@Query("""
+        SELECT p FROM Product p
+        LEFT JOIN FETCH p.seller
+        WHERE p.id = :id
+    """)
+	Optional<Product> findByIdWithSeller(@Param("id") Long id);
+
+	@EntityGraph(attributePaths = {"seller"})
+	Page<Product> findByCategory(Category category, Pageable pageable);
+}
