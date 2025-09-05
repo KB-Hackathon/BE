ea50173bbcc94ec8f6bb37c90231e197bef4b84e
greensnapback0229 <smdmim@gmail.com>
2025-09-05 12:00:14 +0900

    feat: 쿠폰 조회기능 추가

--- src/main/java/hackathon/kb/chakchak/domain/order/repository/OrderRepository.java
+++ src/main/java/hackathon/kb/chakchak/domain/order/repository/OrderRepository.java
@@ -1,14 +1,19 @@
 package hackathon.kb.chakchak.domain.order.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import hackathon.kb.chakchak.domain.member.domain.entity.Member;
 import hackathon.kb.chakchak.domain.order.domain.entity.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
 	Optional<Order> findById(Long id);
+
+	List<Order> findByMember(Member member);
+
 }
--- src/main/java/hackathon/kb/chakchak/domain/product/repository/ProductRepository.java
+++ src/main/java/hackathon/kb/chakchak/domain/product/repository/ProductRepository.java
@@ -1,14 +1,18 @@
 package hackathon.kb.chakchak.domain.product.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import hackathon.kb.chakchak.domain.member.domain.entity.Member;
 import hackathon.kb.chakchak.domain.product.domain.entity.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	Optional<Product> findById(long id);
+
+	List<Product> findByMember(Member member);
 }
