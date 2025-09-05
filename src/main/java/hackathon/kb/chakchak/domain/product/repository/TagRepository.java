de7e4eeeb08e9943bff9766ff26a457f098e1b27
answl <dms2107@sookmyung.ac.kr>
2025-09-05 17:51:57 +0900

    refact: 새 상품 저장 로직 수정

--- src/main/java/hackathon/kb/chakchak/domain/product/repository/TagRepository.java
+++ src/main/java/hackathon/kb/chakchak/domain/product/repository/TagRepository.java
@@ -1,7 +1,9 @@
 package hackathon.kb.chakchak.domain.product.repository;
 
+import hackathon.kb.chakchak.domain.product.domain.entity.Product;
 import hackathon.kb.chakchak.domain.product.domain.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    void deleteByProduct(Product product);
 }
