d42253e51a886300bf42aa0750e8164a12467147
seoyeon2001 <syeon1250@naver.com>
2025-09-05 21:10:01 +0900

    fix(MemberController): 응답객체 통일 BaseResponse

--- src/main/java/hackathon/kb/chakchak/domain/member/api/controller/MemberController.java
+++ src/main/java/hackathon/kb/chakchak/domain/member/api/controller/MemberController.java
@@ -3,8 +3,9 @@ package hackathon.kb.chakchak.domain.member.api.controller;
 import hackathon.kb.chakchak.domain.auth.MemberPrincipal;
 import hackathon.kb.chakchak.domain.member.api.dto.res.MemberProfileResponse;
 import hackathon.kb.chakchak.domain.member.service.MemberService;
+import hackathon.kb.chakchak.global.response.BaseResponse;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,8 @@ public class MemberController {
 
     // 로그인한 사옹자 정보 가져오기
     @GetMapping("/me")
-    public ResponseEntity<MemberProfileResponse> getMyProfile(@AuthenticationPrincipal MemberPrincipal principal) {
+    public BaseResponse<MemberProfileResponse> getMyProfile(@AuthenticationPrincipal MemberPrincipal principal) {
         Long memberId = principal.getId();
-        return ResponseEntity.ok(memberService.getMyProfile(memberId));
+        return BaseResponse.OK(memberService.getMyProfile(memberId));
     }
 }
