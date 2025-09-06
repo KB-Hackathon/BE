e1df8f2d6a9b8da783c7b2d3612d9b59009392f3
Hyunji-JANG <78188222+Hyunji-JANG@users.noreply.github.com>
2025-09-07 00:08:51 +0900

    docs(report): swagger 추가

--- src/main/java/hackathon/kb/chakchak/domain/report/api/controller/ReportController.java
+++ src/main/java/hackathon/kb/chakchak/domain/report/api/controller/ReportController.java
@@ -8,15 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 import hackathon.kb.chakchak.domain.report.api.dto.ReportResponseDto;
 import hackathon.kb.chakchak.domain.report.service.ReportService;
 import hackathon.kb.chakchak.global.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/sellers/report")
 @RequiredArgsConstructor
+@Tag(name = "리포트 관련 API", description = "개별 판매자 리포트 API")
 public class ReportController {
 
 	private final ReportService reportService;
 
+	@Operation(summary = "리포트 조회", description = "판매자 id로 리포트를 조회합니다.")
 	@GetMapping("/{sellerId}")
 	public BaseResponse<ReportResponseDto> getReport(@PathVariable Long sellerId) {
 		return reportService.getReportBySellerId(sellerId);
--- src/main/java/hackathon/kb/chakchak/domain/report/api/dto/ReportResponseDto.java
+++ src/main/java/hackathon/kb/chakchak/domain/report/api/dto/ReportResponseDto.java
@@ -2,17 +2,29 @@ package hackathon.kb.chakchak.domain.report.api.dto;
 
 import java.util.Map;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class ReportResponseDto {
-	private Integer totalSales;     // 총 매출액
-	private Integer successCnt;     // 성공 횟수
-	private Integer failedCnt;      // 실패 횟수
-	private Integer successRate;     // 성공률 (%)
 
-	private Map<String, Integer> ageDistribution;   // 연령대 분포 (%)
-	private Map<String, Integer> genderDistribution; // 성별 분포 (%)
+	@Schema(description = "총 매출액")
+	private Integer totalSales;
+
+	@Schema(description = "성공 횟수")
+	private Integer successCnt;
+
+	@Schema(description = "실패 횟수")
+	private Integer failedCnt;
+
+	@Schema(description = "성공률 (%)")
+	private Integer successRate;
+
+	@Schema(description = "연령대 분포 (%)")
+	private Map<String, Integer> ageDistribution;
+
+	@Schema(description = "성별 분포 (%)")
+	private Map<String, Integer> genderDistribution;
 }
