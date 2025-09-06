67226e48e81d01f48f6496b30bb21c59892cbdd5
answl <dms2107@sookmyung.ac.kr>
2025-09-06 15:22:20 +0900

    docs: swagger 문서 수정

--- src/main/java/hackathon/kb/chakchak/domain/member/api/controller/SellerController.java
+++ src/main/java/hackathon/kb/chakchak/domain/member/api/controller/SellerController.java
@@ -5,6 +5,7 @@ import hackathon.kb.chakchak.domain.member.api.dto.req.BizRegisterRequest;
 import hackathon.kb.chakchak.domain.member.api.dto.res.BizRegisterResponse;
 import hackathon.kb.chakchak.domain.member.domain.entity.Seller;
 import hackathon.kb.chakchak.domain.member.service.SellerService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 @RequestMapping("/api/sellers")
 @RequiredArgsConstructor
-@Tag(name = "사업자 등록 API", description = "에이픽 API와 주소 API 호출 후 사업자 등록 번호 확인 및 행정동 코드 추출 API")
+@Tag(name = "SELLER API", description = "판매자(사업자) 관련 API")
 public class SellerController {
     private final SellerService sellerService;
 
+    @Operation(summary = "사업자 등록", description = "사업자 등록번호를 토대로 관련된 사업자 정보를 반환합니다.")
     @PostMapping("/register")
     public ResponseEntity<BizRegisterResponse> register(
             @Valid @RequestBody BizRegisterRequest req,
