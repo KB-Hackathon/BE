f6443006ecdeb9eb4a19a113485620668c89a4b4
answl <dms2107@sookmyung.ac.kr>
2025-09-06 17:03:59 +0900

    refact: seller, product 관련 dto 수정

--- src/main/java/hackathon/kb/chakchak/domain/product/util/ProductToDtoMapper.java
+++ src/main/java/hackathon/kb/chakchak/domain/product/util/ProductToDtoMapper.java
@@ -1,38 +1,54 @@
 package hackathon.kb.chakchak.domain.product.util;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
+import hackathon.kb.chakchak.domain.member.api.dto.res.SellerReadResponseDto;
 import hackathon.kb.chakchak.domain.member.domain.entity.Seller;
 import hackathon.kb.chakchak.domain.order.domain.entity.Order;
-import hackathon.kb.chakchak.domain.product.domain.dto.ImageReadResponseDto;
-import hackathon.kb.chakchak.domain.product.domain.dto.ProductMemberReadResponseDto;
-import hackathon.kb.chakchak.domain.product.domain.dto.ProductReadResponseDto;
-import hackathon.kb.chakchak.domain.product.domain.dto.TagReadResponseDto;
+import hackathon.kb.chakchak.domain.product.domain.dto.*;
 import hackathon.kb.chakchak.domain.product.domain.entity.Image;
 import hackathon.kb.chakchak.domain.product.domain.entity.Product;
 import hackathon.kb.chakchak.domain.product.domain.entity.Tag;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductToDtoMapper {
 
 	public static ProductReadResponseDto productToProductReadResponseDto(Product product) {
-		Short presentPersonCount = getPresentPersonCount(product.getOrders());
 		return ProductReadResponseDto.builder()
-			.productId(product.getId())
-			.category(product.getCategory())
-			.description(product.getDescription())
-			.price(product.getPrice())
-			.title(product.getTitle())
-			.tags(TagToTagReadResponseDtoList(product.getTags()))
-			.images(ImageToImageReadResponseDto(product.getImages()))
-			.recruitmentEndPeriod(product.getRecruitmentEndPeriod())
-			.recruitmentStartPeriod(product.getRecruitmentStartPeriod())
-			.seller(memberToProductMemberReadResponseDto(product.getSeller()))
-			.targetAmount(product.getTargetAmount())
-			.presentPersonCount(presentPersonCount)
-			.totalPrice(getTotalPrice(presentPersonCount, product.getPrice()))
-			.build();
+				.seller(memberToProductMemberReadResponseDto(product.getSeller()))
+				.product(productToProductSimpleResponseDto(product))
+				.build();
+	}
+
+	public static ProductSimpleResponseDto productToProductSimpleResponseDto(Product product) {
+		Short presentPersonCount = getPresentPersonCount(product.getOrders());
+		return ProductSimpleResponseDto.builder()
+				.productId(product.getId())
+				.category(product.getCategory())
+				.description(product.getDescription())
+				.price(product.getPrice())
+				.title(product.getTitle())
+				.tags(TagToTagReadResponseDtoList(product.getTags()))
+				.images(ImageToImageReadResponseDto(product.getImages()))
+				.recruitmentEndPeriod(product.getRecruitmentEndPeriod())
+				.recruitmentStartPeriod(product.getRecruitmentStartPeriod())
+				.targetAmount(product.getTargetAmount())
+				.presentPersonCount(presentPersonCount)
+				.totalPrice(getTotalPrice(presentPersonCount, product.getPrice()))
+				.build();
+	}
+
+	public static SellerReadResponseDto sellerToSellerProductsResponseDto(Seller seller) {
+		return SellerReadResponseDto.builder()
+				.seller(memberToProductMemberReadResponseDto(seller))
+				.products(
+						seller.getProducts()
+								.stream()
+								.map(ProductToDtoMapper::productToProductSimpleResponseDto)
+								.toList()
+				)
+				.build();
 	}
 
 	public static List<TagReadResponseDto> TagToTagReadResponseDtoList(List<Tag> tags) {
