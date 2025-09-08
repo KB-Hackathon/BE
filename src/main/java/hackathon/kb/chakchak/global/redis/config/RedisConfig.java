bca2304cc449fb06bcf64b72d5f9941c8756b76d
answl <dms2107@sookmyung.ac.kr>
2025-09-08 21:36:17 +0900

    fix: propertySource 제거

--- src/main/java/hackathon/kb/chakchak/global/redis/config/RedisConfig.java
+++ src/main/java/hackathon/kb/chakchak/global/redis/config/RedisConfig.java
@@ -2,20 +2,19 @@ package hackathon.kb.chakchak.global.redis.config;
 
 import io.lettuce.core.resource.ClientResources;
 import io.lettuce.core.resource.DefaultClientResources;
-import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.beans.factory.annotation.Value;
+
+import java.time.Duration;
 
 @Configuration
-@PropertySource("classpath:application.yml")
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
