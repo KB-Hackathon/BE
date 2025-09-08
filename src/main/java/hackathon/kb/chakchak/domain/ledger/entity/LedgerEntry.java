b90bf7a6d419c5aaec03d76e4d1975da0ec2d842
greensnapback0229 <smdmim@gmail.com>
2025-09-08 20:35:48 +0900

    feat: 회계전표 맵핑관계 수정

--- src/main/java/hackathon/kb/chakchak/domain/ledger/entity/LedgerEntry.java
+++ src/main/java/hackathon/kb/chakchak/domain/ledger/entity/LedgerEntry.java
@@ -5,13 +5,14 @@ import java.math.BigDecimal;
 import hackathon.kb.chakchak.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -21,32 +22,29 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Builder
 @Table(name = "ledger_entry")
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class LedgerEntry extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ledger_entry_id")
 	private Long ledgerEntryId;
+
+	@Column(nullable = false, precision = 19, scale = 2)
 	private BigDecimal amount;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "ledger_type", nullable = false, length = 10)
 	private LedgerType ledgerType;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "ledger_voucher_id")
+	@JoinColumn(name = "ledger_voucher_id", nullable = false)
 	private LedgerVoucher ledgerVoucher;
 
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "ledger_code_id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ledger_code_id", nullable = false)
 	private LedgerCode ledgerCode;
-
-	@Builder
-	public LedgerEntry(BigDecimal amount, LedgerType ledgerType, LedgerVoucher ledgerVoucher, LedgerCode ledgerCode) {
-		this.amount = amount;
-		this.ledgerType = ledgerType;
-		this.ledgerVoucher = ledgerVoucher;
-		this.ledgerCode = ledgerCode;
-	}
 }
