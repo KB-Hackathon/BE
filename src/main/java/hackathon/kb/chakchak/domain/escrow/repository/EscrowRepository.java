d339ef59492027ca5094daee5bdf11c25a8d65d8
greensnapback0229 <smdmim@gmail.com>
2025-09-08 21:27:35 +0900

    fix: repository 변경

--- src/main/java/hackathon/kb/chakchak/domain/escrow/repository/EscrowRepository.java
+++ src/main/java/hackathon/kb/chakchak/domain/escrow/repository/EscrowRepository.java
@@ -1,10 +1,10 @@
 package hackathon.kb.chakchak.domain.escrow.repository;
 
-import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import hackathon.kb.chakchak.domain.escrow.domain.entity.Escrow;
 
 @Repository
-public interface EscrowRepository extends JpaAttributeConverter<Escrow, Long> {
+public interface EscrowRepository extends JpaRepository<Escrow, Long> {
 }
