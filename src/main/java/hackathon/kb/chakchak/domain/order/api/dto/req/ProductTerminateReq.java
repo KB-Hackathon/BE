ab3444d10cffd633cf633244534e62663889d18c
greensnapback0229 <smdmim@gmail.com>
2025-09-09 20:44:55 +0900

    fix: 상품 생성시 에스크로 테이블 추가

---
+++ src/main/java/hackathon/kb/chakchak/domain/order/api/dto/req/ProductTerminateReq.java
@@ -0,0 +1,5 @@
+package hackathon.kb.chakchak.domain.order.api.dto.req;
+
+public record ProductTerminateReq(Long productId) {
+
+}
--- src/main/java/hackathon/kb/chakchak/domain/product/domain/entity/Product.java
+++ src/main/java/hackathon/kb/chakchak/domain/product/domain/entity/Product.java
@@ -1,18 +1,36 @@
 package hackathon.kb.chakchak.domain.product.domain.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hibernate.annotations.BatchSize;
+
 import hackathon.kb.chakchak.domain.escrow.domain.entity.Escrow;
 import hackathon.kb.chakchak.domain.member.domain.entity.Seller;
 import hackathon.kb.chakchak.domain.order.domain.entity.Order;
 import hackathon.kb.chakchak.domain.product.domain.enums.Category;
 import hackathon.kb.chakchak.domain.product.domain.enums.ProductStatus;
 import hackathon.kb.chakchak.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.annotations.BatchSize;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -88,6 +106,10 @@ public class Product extends BaseEntity {
 	private LocalDateTime refreshedAt;
 
 	// ====== 변경 메서드 (업데이트 전용) ======
+
+	public void updateEscrow(Escrow escrow) {
+		this.escrow = escrow;
+	}
 
 	public void changeEndCaptureId(Long id) {
 		this.endCaptureId = id;
